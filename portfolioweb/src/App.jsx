31de42d95803055b9525f26acbbab10a0cbb8f35
cristalPyC <cristaltn03@gmail.com>
2023-11-17 11:54:39 -0400

    Adding modules

--- portfolioweb/src/App.jsx
+++ portfolioweb/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <p>Portfolio in progress</p>
     </>
-  )
+  );
 }
 
 export default App
