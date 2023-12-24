546895aec06a9029493a2ed3387297413f4603cc
cristalPyC <cristaltn03@gmail.com>
2023-12-24 15:20:59 -0400

    Adding de CV link

--- portfolioweb/src/pages/about-me/AboutMe.jsx
+++ portfolioweb/src/pages/about-me/AboutMe.jsx
@@ -23,7 +23,7 @@ export const AboutMe = () => {
             </h3>
           </div>
           <hr />
-          <button>{texts.cvTitle}</button>
+          <a href="https://drive.google.com/file/d/1BR6sJpGZYbW1NanQIbqhQmibyyfWl_Ir/view?usp=sharing"><button>{texts.cvTitle}</button></a>
       </div>
     </div>
   )
