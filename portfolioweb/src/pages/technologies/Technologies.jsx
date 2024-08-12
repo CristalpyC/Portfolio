e093f932b76a0afe53c877c85753af5fa461340c
cristalPyC <cristaltn03@gmail.com>
2024-08-12 11:39:44 -0400

    fixed

--- portfolioweb/src/pages/technologies/Technologies.jsx
+++ portfolioweb/src/pages/technologies/Technologies.jsx
@@ -1,16 +1,4 @@
 import './technologiesStyle.css';
-import htmlImg from '../../imgs/html3dIcon.webp';
-import cssImg from '../../imgs/css3dIcon.webp';
-import jsImg from '../../imgs/JS3dIcon.webp';
-import reactImg from '../../imgs/react3dIcon.png';
-import bootstrapImg from '../../imgs/bootstrap3dIcon.webp';
-import pythonImg from '../../imgs/python3dIcon.webp';
-import gitImg from '../../imgs/git3dIcon.webp';
-import sqlImg from '../../imgs/sql3dIcon.png';
-import scssImg from '../../imgs/scss-3d.png';
-import nextImg from '../../imgs/next-3d.png';
-import uiImg from '../../imgs/materialUi3dImg.png';
-import figmaImg from '../../imgs/figma3dicon.webp';
 import { useContext } from 'react';
 import LanguageContext from '../../hooks/useLanguage';
 import { Zoom } from "react-awesome-reveal";
@@ -18,75 +6,31 @@ import { Zoom } from "react-awesome-reveal";
 export const Technologies = () => {
   const {texts} = useContext(LanguageContext);
   const title = `<${texts.techTitle}/>`
+  const technologies = [
+    {name: 'HTML', src:"html3dIcon.webp"},
+    {name: 'CSS', src:"css3dIcon.webp"},
+    {name: 'JavaScript',src:"JS3dIcon.webp"},
+    {name: 'React.Js',src:"react3dIcon.png"},
+    {name: 'Bootsrap', src:"bootstrap3dIcon.webp"},
+    {name: 'Python',src:"python3dIcon.webp"},
+    {name: 'Git',src:"git3dIcon.webp"},
+    {name: 'MS SQL server',src:"sql3dIcon.png"},
+    {name: 'Material UI',src:"materialUi3dImg.png"},
+    {name: 'Figma', src:"figma3dicon.webp"},
+    {name: 'SCSS', src:"scss3d.png"},
+    {name: 'Next.JS', src:"next-3d.png"},
+    {name: 'Angular', src:"angular-icon.webp"},
+  ]
   return (
     <div id='technologies__container'>
       <Zoom><h2 className='technologies__title'>{title}</h2></Zoom>
         <div className="tech__container" >
-            <div className="technologie">
-                <img src={htmlImg} alt="" />
-                <h3>HTML</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={cssImg} alt="" />
-                <h3>CSS</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={jsImg} alt="" />
-                <h3>JavaScript</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={reactImg} alt="" />
-                <h3>React.Js</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={bootstrapImg} alt="" />
-                <h3>Bootsrap</h3>
-            </div>
-
-
-            <div className="technologie">
-                <img src={pythonImg} alt="" />
-                <h3>Python</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={gitImg} alt="" />
-                <h3>Git</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={sqlImg} alt="" />
-                <h3>MS SQL server</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={uiImg} alt="" />
-                <h3>Material UI</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={figmaImg} alt="" />
-                <h3>Figma</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={scssImg} alt="" />
-                <h3>SCSS</h3>
-            </div>
-
-            <div className="technologie">
-                <img src={nextImg} alt="" />
-                <h3>Next.JS</h3>
-            </div>
-
-            <div className="technologie">
-                <img src="angular-icon.webp" alt="" />
-                <h3>Angular</h3>
-            </div>
+            {technologies.map((items, index) => (
+                <div className="technologie" key={index}>
+                    <img src={items.src} alt="" />
+                    <h3>{items.name}</h3>
+                </div>
+            ))}
         </div>
     </div>
   )
